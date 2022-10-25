e6fe7822503801f6d943e945541ebe536ce1d51d
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-10-25 22:20:11 +1100

    set up 'db' and card model

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
 def create_app():
     # using a list comprehension and multiple assignment 
     # to grab the environment variables we need
@@ -11,8 +13,8 @@ def create_app():
 
     # configuring our app:
     app.config.from_object("config.app_config")
-
-    # creating our database object! This allows us to use our ORM
-    db = SQLAlchemy(app)
     
+    # creating our database object! This allows us to use our ORM
+    db.init_app(app)
+
     return app
