f599a9732e23e3e153a6e7e4829e20421098a2f7
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-10-25 22:02:26 +1100

    base app

---
+++ main.py
@@ -0,0 +1,18 @@
+# https://github.com/CoderAcademy-ALL/build_a_flask_api_lesson
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+def create_app():
+    # using a list comprehension and multiple assignment 
+    # to grab the environment variables we need
+    
+    # Creating the flask app object - this is the core of our app!
+    app = Flask(__name__)
+
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating our database object! This allows us to use our ORM
+    db = SQLAlchemy(app)
+    
+    return app
