db9f3fa0bc73190993d1a41fb41c1c24905fff32
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-10-26 23:43:26 +1100

    added create card route, returns user details

--- schemas/card_schema.py
+++ schemas/card_schema.py
@@ -1,10 +1,13 @@
 from main import ma
+from marshmallow import fields
 
 #create the Card Schema with Marshmallow, it will provide the serialization needed for converting the data into JSON
 class CardSchema(ma.Schema):
+    user = fields.Nested("UserSchema", only=("email",))
+
     class Meta:
         # Fields to expose
-        fields = ("id", "title", "description", "date", "status", "priority")
+        fields = ("id", "title", "description", "date", "status", "priority", "user")
 
 #single card schema, when one card needs to be retrieved
 card_schema = CardSchema()
