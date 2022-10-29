6f300ac402860bdbd9ca318eec83f725274bdaa8
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-10-30 00:45:30 +1100

    added custom validation for >1 Ongoing cards

--- schemas/card_schema.py
+++ schemas/card_schema.py
@@ -1,6 +1,8 @@
 from main import ma
-from marshmallow import fields
+from marshmallow import fields,validates
 from marshmallow.validate import Length, OneOf, Regexp, And
+from marshmallow.exceptions import ValidationError
+from models.cards import Card
 
 VALID_PRIORITIES = ('Urgent', 'High', 'Low', 'Medium')
 VALID_STATUSES = ('To Do', 'Done', 'Ongoing', 'Testing', 'Deployed')
@@ -12,6 +14,16 @@ class CardSchema(ma.Schema):
     title = fields.String(required=True, validate=And(Length(min=1), Regexp('^[a-zA-Z0-9 ]+$')))    
     status = fields.String(required=True, validate=OneOf(VALID_STATUSES))
     priority = fields.String(load_default='Medium', validate=OneOf(VALID_PRIORITIES))        
+
+    @validates('status')
+    def validate_status(self, value):
+        # Only apply this validator if the attempted status is 'Ongoing'
+        if value == 'Ongoing':
+            # Get a count of cards that already have the 'Ongoing' status
+            count = Card.query.filter_by(status='Ongoing').count()
+            if count > 1:
+                raise ValidationError('You already have an ongoing card')
+
     class Meta:
         ordered = True
         # Fields to expose
